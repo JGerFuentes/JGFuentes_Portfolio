f3c6bcdc200f62a14af5f5c5cb3d0a86a29037f6
JGerFuentes <jgerfuentes@gmail.com>
2024-05-13 05:22:20 -0300

    Structuring and designing the About component

--- minimal-portfolio/src/components/About.jsx
+++ minimal-portfolio/src/components/About.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <article>
+        <header>
+            <img src="" alt="profile_pic" title="JGF"/>
+            <h1>
+                Hi, there! 👋🏼 My name's Jose German Fuentes
+            </h1>
+            <h2>
+                Full Stack Web Developer
+            </h2>
+        </header>
+        <main>
+            <p>Brief description, goals, etc...</p>
+        </main>
+        <section>
+            <button>LinkedIn</button>
+            <button>Github</button>
+            <button>Resume</button>
+            <button>Este no seria un boton, sino mi mail con opcion de "copiar"</button>
+        </section>
+    </article>
+    
   )
 }
 
