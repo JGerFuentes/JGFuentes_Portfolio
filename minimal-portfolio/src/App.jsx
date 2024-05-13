aa19a386c44ad664fc040fcc863b12c5240c0a10
JGerFuentes <jgerfuentes@gmail.com>
2024-05-13 05:21:52 -0300

    Import of components

--- minimal-portfolio/src/App.jsx
+++ minimal-portfolio/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import About from './components/About'
+import Experience from './components/Experience'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Stack from './components/Stack'
+
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <main>
+      <Navbar/>
+      <About/>
+      <Experience/>
+      <Stack/>
+      <Projects/>
+    </main>
   )
 }
 
