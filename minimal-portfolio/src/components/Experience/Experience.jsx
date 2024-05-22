413608142cda413dc9f8b4a1bd376d94edeff30a
JGerFuentes <jgerfuentes@gmail.com>
2024-05-22 05:29:52 -0300

    Writing Experience component content

--- minimal-portfolio/src/components/Experience/Experience.jsx
+++ minimal-portfolio/src/components/Experience/Experience.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <article>
+      <h2>Experience</h2>
+
+      <section>
+        <h3><strong>Junior Back End Developer</strong></h3>
+        <h4><strong>KaroKids - Internship</strong></h4>
+        <h5>feb. 2024 - mar. 2024</h5>
+        <h5>Medellin, Antioquia, Colombia - Remote</h5>
+        <p> Goals: Increase sales volume by designing a B2C website exclusively dedicated to the promotion and commercialization of the store’s products. Enable direct and fluent communication between business and customers. The website includes: User registration and login protocols, administrator dashboard, user dashboard, shopping cart, favorites list, payment gateway, simple and intuitive interface. Stack: JavaScript, React, Vite, Redux-Toolkit, Tailwind, Firebase, Google Charts, Mercado Pago API, Nodejs, Expressjs, Nodemailer, HTML, CSS, Cloudinary, PostgreSQL, Sequelize</p>
+      </section>
+    </article>
   )
 }
 
