009fced616d30e835e15c55d75afcd28e053f06d
JGerFuentes <jgerfuentes@gmail.com>
2024-06-03 04:56:30 -0300

    General structure of the Stack component

--- minimal-portfolio/src/components/Stack/Stack.jsx
+++ minimal-portfolio/src/components/Stack/Stack.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { SiJavascript, SiHtml5, SiCss3, SiSass, SiReact, SiRedux, SiNodedotjs, SiExpress, SiPostgresql, SiSequelize, SiJest, SiVisualstudiocode, SiGit, SiNpm, SiVercel, SiRapid, SiVitest, SiNodemon, SiFigma, SiSlack, SiTrello, SiNotion, SiC, SiTypescript, SiTailwindcss,  } from "react-icons/si";
 
 const Stack = () => {
   return (
-    <div>Stack</div>
+    <article>
+      <section>
+        <h2>Stack</h2>
+        <ul>
+          <li><SiJavascript /></li>
+          <li><SiHtml5 /></li>
+          <li><SiCss3 /></li>
+          <li><SiSass /></li>
+          <li><SiReact /></li>
+          <li><SiRedux /></li>
+          <li><SiNodedotjs /></li>
+          <li><SiExpress /></li>
+          <li><SiPostgresql /></li>
+          <li><SiSequelize /></li>
+          <li><SiJest /></li>
+        </ul>
+      </section>
+      <section>
+        <h2>Other technologies</h2>
+        <ul>
+          <li><SiVisualstudiocode /></li>
+          <li><SiGit /></li>
+          <li><SiNpm /></li>
+          {/* <li><SiVercel /></li> */}
+          <li><SiRapid /></li>
+          {/* <li><SiVitest /></li> */}
+          <li><SiNodemon /></li>
+          <li><SiFigma /></li>
+          <li><SiSlack /></li>
+          <li><SiTrello /></li>
+          <li><SiNotion /></li>
+        </ul>
+      </section>
+      <section>
+        <h2>Currently learning</h2>
+        <ul>
+          <li><SiC /></li>
+          <li><SiTypescript /></li>
+          <li><SiTailwindcss /></li>
+        </ul>
+      </section>
+    </article>
   )
 }
 
