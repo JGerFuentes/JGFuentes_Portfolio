3eca8e0f92bbcc3e5c9e1df60a873cfb4d144252
JGerFuentes <jgerfuentes@gmail.com>
2024-06-05 03:05:02 -0300

    Creation of the ErrorPage component content

--- minimal-portfolio/src/components/ErrorPage/ErrorPage.jsx
+++ minimal-portfolio/src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,19 @@
 
+
 const ErrorPage = () => {
   return (
     <article>
-        <h2>Error 404!</h2>
-        <img/>
-        <p>Some funny text</p>
+      <h2>Like Schrödinger's cat, this page may or may not be around...</h2>
+
+      <img src="https://i.imgur.com/VC1xvyn.gif" title="404_cat_status" alt="404_cat_status" />
+
+      <p>
+        So... your page was not found... Sorry pal 😔
+        <br/>
+        Please, try again later or double check that you got the right URL.
+        <br/>
+        Keep rolling! 🤘
+      </p>
     </article>
   )
 }
