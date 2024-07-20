4f2e2024108c9818411cd69b1b8ced03d801eef8
JGerFuentes <jgerfuentes@gmail.com>
2024-07-20 04:27:42 -0300

    Word change.

--- minimal-portfolio/src/main.jsx
+++ minimal-portfolio/src/main.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback='Loading an amazing website...'>
+    <React.Suspense fallback='Loading an amazing portfolio...'>
       <BrowserRouter>
         <App />
       </BrowserRouter>
