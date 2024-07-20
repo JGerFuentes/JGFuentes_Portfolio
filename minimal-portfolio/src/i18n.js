ca95b97309b0ce85c34926d0a999a790471f07a2
JGerFuentes <jgerfuentes@gmail.com>
2024-07-20 03:41:34 -0300

    Implementation of the i18next-http-backend plugin in the i18n initialization file

--- minimal-portfolio/src/i18n.js
+++ minimal-portfolio/src/i18n.js
@@ -1,22 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend'; // for passing in lng and translations on init.
 
-// import Backend from 'i18next-http-backend';
-// // don't want to use this?
-// // have a look at the Quick start guide 
-// // for passing in lng and translations on init
+
 
 i18n
     .use(initReactI18next) //we pass the i18n instance to react-i18next which will make it available for all the components.
     .use(LanguageDetector) // Detect user language
-    // .use(Backend) //Allows the use of locales directory with all the needed translations.
+    .use(Backend) //Allows the use of locales directory with all the needed translations.
     
     //init i18next
     .init({ 
         // lng: document.querySelector('html').lang, //Identifies the html language. NOt needed when using LanguageDetector
-        fallbackLng: 'en', //In case there is no translation available.
         debug: true, //For debugging purposes.
+        fallbackLng: 'en', //In case there is no translation available.
         // detection: {
         //     order: ['navigator', 'cookie', 'localStorage', 'sessionStorage', 'path', 'subdomain'],
         // }
