2f9889d60ff4e2319e39f8ffe0e81f07dc0d4df8
JGerFuentes <jgerfuentes@gmail.com>
2024-07-20 04:02:58 -0300

    Implementation of a Suspense component to enhance UX.

--- minimal-portfolio/src/main.jsx
+++ minimal-portfolio/src/main.jsx
@@ -10,8 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <React.Suspense fallback='Loading an amazing website...'>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.Suspense>
   </React.StrictMode>,
 )
