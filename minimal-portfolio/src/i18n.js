c19e4467f48bf7e769521bfe2b9512235486c615
JGerFuentes <jgerfuentes@gmail.com>
2024-08-10 05:27:31 -0300

    Elimination of unnecessary comments

--- minimal-portfolio/src/i18n.js
+++ minimal-portfolio/src/i18n.js
@@ -3,13 +3,11 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend'; // for passing in lng and translations on init.
 
-
-
 i18n
     .use(initReactI18next) //we pass the i18n instance to react-i18next which will make it available for all the components.
     .use(LanguageDetector) // Detect user language
     .use(HttpApi) //Allows the use of locales directory with all the needed translations.
-    
+
     //init i18next
     .init({ 
         supportedLngs: ['en', 'es', 'de', 'fr'],
@@ -18,12 +16,6 @@ i18n
         detection: {
             order: ['localStorage', 'cookie', 'navigator', 'sessionStorage', 'path', 'subdomain'],
         },
-        backend: {
-            loadPath: '/assets/locales/{{lng}}/translation.json',
-        },
-
-        // load: 'languageOnly',
     });
-
 
 export default i18n;
